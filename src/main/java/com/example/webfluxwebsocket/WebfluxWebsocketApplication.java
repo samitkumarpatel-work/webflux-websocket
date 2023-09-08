a433b553c9cc986ac3d1bc8ac1fbc3579fcdfcc7
samitkumarpatel-work <SamitKumar.Patel@maersk.com>
2023-09-08 21:22:58 +0200

    chat example added

--- src/main/java/com/example/webfluxwebsocket/WebfluxWebsocketApplication.java
+++ src/main/java/com/example/webfluxwebsocket/WebfluxWebsocketApplication.java
@@ -1,6 +1,7 @@
 package com.example.webfluxwebsocket;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +9,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @RequiredArgsConstructor
@@ -24,12 +30,13 @@ public class WebfluxWebsocketApplication {
 		SpringApplication.run(WebfluxWebsocketApplication.class, args);
 	}
 
-	private final WebSocketHandler webSocketHandler;
-
+	private final BasicWebSocketHandler basicWebSocketHandler;
+	private final ChatWebSocketHandler chatWebSocketHandler;
 	@Bean
 	public HandlerMapping webSocketHandlerMapping() {
 		Map<String, WebSocketHandler> map = new HashMap<>();
-		map.put("/websocket-emitter", webSocketHandler);
+		map.put("/websocket-emitter", basicWebSocketHandler);
+		map.put("/websocket-chat", chatWebSocketHandler);
 
 		SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
 		handlerMapping.setOrder(1);
@@ -39,17 +46,40 @@ public class WebfluxWebsocketApplication {
 }
 
 @Component
-class ReactiveWebSocketHandler implements WebSocketHandler {
-
+class BasicWebSocketHandler implements WebSocketHandler {
 	@Override
 	public Mono<Void> handle(WebSocketSession session) {
+
+		var f = Flux.interval(Duration.ofSeconds(5))
+				.map(l -> session.textMessage("Hello " + l));
+
 		return session
-				.send(Flux.interval(Duration.ofSeconds(5))
-						.map(l -> session.textMessage("Hello " + l))
-				)
-				.and(session.receive()
+				.send(f)
+				.and(session
+						.receive()
 						.map(msg -> "RECEIVED ON SERVER :: " + msg.getPayloadAsText())
+						.map(session::textMessage)
+						.flatMap(s -> session.send(Mono.just(s)))
 						.log()
 				);
+	}
+}
+
+@Component
+@Slf4j
+class ChatWebSocketHandler implements WebSocketHandler {
+	Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
+	@Override
+	public Mono<Void> handle(WebSocketSession session) {
+		log.info("Session established: id: {}, isOpen: {} " , session.getId(), session.isOpen());
+
+		sink.tryEmitNext("%s has joined the chat".formatted(session.getId()));
+		
+		return session
+				.send(sink.asFlux().map(session::textMessage))
+				.and(session.receive()
+						.map(WebSocketMessage::getPayloadAsText)
+						.doOnNext(sink::tryEmitNext)
+						.then());
 	}
 }
